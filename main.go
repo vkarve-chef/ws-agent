c2a167f93995ba1225741f216f36c6f29b692a68
Vikram Karve <vikram.karve@progress.com>
2022-06-09 14:38:00 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		os.Exit(0)
+	}()
+
+	for range time.Tick(5 * time.Second) {
+		fmt.Println("tick!")
+	}
+}
